a7167f7ba13649009bf32a30e47c25146be9a9e6
Mark Paschal <markpasc@markpasc.org>
2009-11-20 12:02:16 -0800

    Add 'info' command to find a distribution from a PyPI server

--- pip.py
+++ pip.py
@@ -1186,6 +1186,104 @@ class UnzipCommand(ZipCommand):
 
 UnzipCommand()
 
+class InfoCommand(Command):
+    name = 'info'
+    usage = '%prog [OPTIONS] DISTRIBUTION_NAMES...'
+    summary = 'Get info from PyPI about distributions'
+
+    def __init__(self):
+        super(InfoCommand, self).__init__()
+
+        self.parser.add_option(
+            '-i', '--index-url', '--pypi-url',
+            dest='index_url',
+            metavar='URL',
+            default='http://pypi.python.org/pypi',
+            help='Base URL of Python Package Index (default %default)')
+        self.parser.add_option(
+            '--extra-index-url',
+            dest='extra_index_urls',
+            metavar='URL',
+            action='append',
+            default=[],
+            help='Extra URLs of package indexes to use in addition to --index-url')
+
+    def run(self, options, args):
+        indexes = [options.index_url] + options.extra_index_urls
+
+        release_data = dict()
+
+        for index_url in indexes:
+            for dist in args:
+                try:
+                    release_data[dist] = self.release_in_index(dist, index_url)
+                except DistributionNotFound:
+                    pass
+
+        for dist in args:
+            try:
+                self.show_release_data(release_data[dist])
+            except KeyError:
+                print "Found no such release %r" % dist
+
+    def release_in_index(self, dist, index_url):
+        import xmlrpclib
+        server = xmlrpclib.Server(index_url)
+
+        req = pkg_resources.Requirement.parse(dist)
+        logger.debug('Requirement for %r is %r', dist, req)
+
+        releases = server.package_releases(dist)
+        logger.debug('Checking if %r are in %r', releases, req)
+        good_releases = [r for r in releases if r in req]
+        if not good_releases:
+            logger.debug('Oops, none of %r are in %r', releases, req)
+            raise DistributionNotFound()
+
+        logger.debug('Yay, %r are in %r', good_releases, req)
+        raw_ver_for_parsed_ver = dict((pkg_resources.parse_version(r), r)
+            for r in good_releases)
+        parsed_versions = sorted(raw_ver_for_parsed_ver.keys())
+        best_version = parsed_versions[-1]
+        logger.debug('Best of %r is %r', parsed_versions, best_version)
+
+        best_raw_version = raw_ver_for_parsed_ver[best_version]
+        logger.debug('Yay, asking for %r %r', req.project_name, best_raw_version)
+        release = server.release_data(req.project_name, best_raw_version)
+        return release
+
+    def show_release_data(self, data):
+        lines = list()
+        lines.append("%(name)s %(version)s - %(summary)s")
+        lines.append("")
+
+        if data['author'] and data['author_email']:
+            lines.append("Author: %(author)s <%(author_email)s>")
+        elif data['author']:
+            lines.append("Author: %(author)s")
+        if data['maintainer'] and data['maintainer_email']:
+            lines.append("Maintainer: %(maintainer)s <%(maintainer_email)s>")
+        elif data['maintainer']:
+            lines.append("Maintainer: %(maintainer)s")
+
+        if data['home_page']:
+            lines.append("Home page: %(home_page)s")
+        lines.append("License: %(license)s")
+        lines.append("")
+
+        # These are lists, so stringify them here.
+        if data['provides']:
+            lines.append("Provides: %s" % ', '.join(data['provides']))
+        if data['requires']:
+            lines.append("Requires: %s" % ', '.join(data['requires']))
+        if data['obsoletes']:
+            lines.append("Obsoletes: %s" % ', '.join(data['obsoletes']))
+
+        for line in lines:
+            print line % data
+
+InfoCommand()
+
 BASE_COMPLETION = """
 # pip %(shell)s completion start%(script)s# pip %(shell)s completion end
 """
