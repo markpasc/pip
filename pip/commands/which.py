3ca4e35b909ae67fd2d5ff2c7e5ca6f848892d42
Mark Paschal <markpasc@markpasc.org>
2009-11-30 14:26:54 -0800

    Mangle into a real pip command

--- pip/commands/which.py
+++ pip/commands/which.py
@@ -1,102 +1,79 @@
-#!/usr/bin/env python
-
-import logging
-from optparse import OptionParser
 import os
 import sys
-
-DOT = '.'
-
-log = logging.getLogger('pywhich')
-
-
-class ModuleNotFound(Exception):
-    pass
+import pip
+from pip.log import logger
+from pip.basecommand import Command
+from pip.exceptions import DistributionNotFound
 
 
-def identify_module(arg, real_path=None, show_directory=None,
-    find_source=None, hide_init=None):
-    try:
-        __import__(arg)
-    except Exception, exc:
-        raise ModuleNotFound("%s: %s" % (type(exc).__name__, str(exc)))
+class WhichCommand(Command):
 
-    mod = sys.modules[arg]
-    filename = mod.__file__
+    name = 'which'
+    usage = '%prog [OPTIONS] MODULE_NAME'
+    summary = 'Find where a module is installed'
 
-    if find_source and (filename.endswith('.pyc') or filename.endswith('.pyo')):
-        sourcefile = filename[:-1]
-        if os.access(sourcefile, os.F_OK):
-            filename = sourcefile
+    def __init__(self):
+        super(WhichCommand, self).__init__()
 
-    if real_path:
-        filename = os.path.realpath(filename)
+        self.parser.add_option('-r', action="store_true", dest="real_path",
+            default=False, help="dereference symlinks")
+        self.parser.add_option('-b', action="store_true", dest="show_directory",
+            default=False, help="show directory instead of filename")
+        self.parser.add_option('-i', '--hide-init', action="store_true", dest="hide_init",
+            default=False, help="show directory if the module ends in __init__.py")
+        self.parser.add_option('--source', action="store_true", dest="find_source",
+            default=False, help="find .py files for .pyc/.pyo files")
 
-    if show_directory or (hide_init and
-        os.path.basename(filename).startswith('__init__.')):
-        filename = os.path.dirname(filename)
+    def run(self, options, args):
+        kwargs = dict((fld, getattr(options, fld)) for fld
+            in ('real_path', 'show_directory', 'find_source', 'hide_init'))
+        self.identify_modules(*args, **kwargs)
 
-    return filename
-
-
-def identify_modules(*args, **kwargs):
-    if len(args) == 1:
-        path_template = "%(file)s"
-        error_template = "Module '%(mod)s' not found (%(error)s)"
-    else:
-        path_template = "%(mod)s: %(file)s"
-        error_template = "%(mod)s: not found (%(error)s)"
-
-    for modulename in args:
+    def identify_module(self, arg, real_path=None, show_directory=None,
+        find_source=None, hide_init=None):
         try:
-            filepath = identify_module(modulename, **kwargs)
-        except ModuleNotFound, exc:
-            print >>sys.stderr, error_template % {
-                'mod': modulename,
-                'error': str(exc),
-            }
+            __import__(arg)
+        except Exception, exc:
+            raise DistributionNotFound("%s: %s" % (type(exc).__name__, str(exc)))
+
+        mod = sys.modules[arg]
+        filename = mod.__file__
+
+        if find_source and (filename.endswith('.pyc') or filename.endswith('.pyo')):
+            sourcefile = filename[:-1]
+            if os.access(sourcefile, os.F_OK):
+                filename = sourcefile
+
+        if real_path:
+            filename = os.path.realpath(filename)
+
+        if show_directory or (hide_init and
+            os.path.basename(filename).startswith('__init__.')):
+            filename = os.path.dirname(filename)
+
+        return filename
+
+    def identify_modules(self, *args, **kwargs):
+        if len(args) == 1:
+            path_template = "%(file)s"
+            error_template = "Module '%(mod)s' not found (%(error)s)"
         else:
-            print path_template % {
-                'mod': modulename,
-                'file': filepath
-            }
+            path_template = "%(mod)s: %(file)s"
+            error_template = "%(mod)s: not found (%(error)s)"
+
+        for modulename in args:
+            try:
+                filepath = self.identify_module(modulename, **kwargs)
+            except DistributionNotFound, exc:
+                print >>sys.stderr, error_template % {
+                    'mod': modulename,
+                    'error': str(exc),
+                }
+            else:
+                print path_template % {
+                    'mod': modulename,
+                    'file': filepath
+                }
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-
-    parser = OptionParser()
-
-    parser.add_option('-v', '--verbose', dest="verbose", action="count",
-        default=2, help="be chattier (stackable)")
-    def quiet(option, opt_str, value, parser):
-        parser.values.verbose -= 1
-    parser.add_option('-q', '--quiet', action="callback", callback=quiet,
-        help="be less chatty (stackable)")
-
-    parser.add_option('-r', action="store_true", dest="real_path",
-        default=False, help="dereference symlinks")
-    parser.add_option('-b', action="store_true", dest="show_directory",
-        default=False, help="show directory instead of filename")
-    parser.add_option('-i', '--hide-init', action="store_true", dest="hide_init",
-        default=False, help="show directory if the module ends in __init__.py")
-    parser.add_option('-s', '--source', action="store_true", dest="find_source",
-        default=False, help="find .py files for .pyc/.pyo files")
-
-    opts, args = parser.parse_args()
-
-    log_levels = (logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG)
-    logging.basicConfig()
-    log.setLevel(log_levels[opts.verbose % 5])
-
-    kwargs = dict((fld, getattr(opts, fld)) for fld
-        in ('real_path', 'show_directory', 'find_source', 'hide_init'))
-    identify_modules(*args, **kwargs)
-
-    return 0
-
-
-if __name__ == '__main__':
-    sys.exit(main())
-
+WhichCommand()
